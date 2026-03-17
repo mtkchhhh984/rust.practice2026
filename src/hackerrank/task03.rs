87363f44fbcc7fcf79ad6d5e120ea4961ec1c7b7
mtkchhhh984 <marta.tkachuk@e-u.edu.ua>
2026-03-17 03:40:53 +0200

    add task03 staircase

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,23 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+fn staircase(n: i32) -> String {
+    let mut result = String::new();
+
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+        result.push_str(&spaces);
+        result.push_str(&hashes);
+
+        if i < n {
+            result.push('\n');
+        }
+    }
+
+    result
+}
+
+#[test]
+fn test3() {
+    let expected = "     #\n    ##\n   ###\n  ####\n #####\n######";
+    let real = staircase(6);
+    assert_eq!(real, expected);
+}

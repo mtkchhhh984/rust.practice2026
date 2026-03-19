15108d840af023333dac79264c0e98fbecad5414
mtkchhhh984 <marta.tkachuk@e-u.edu.ua>
2026-03-19 02:51:45 +0200

    add task06 kangaroo

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,26 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        return "NO".to_string();
+    }
+
+    if (x2 - x1) % (v1 - v2) == 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
+#[test]
+fn test6_yes() {
+    let real = kangaroo(0, 3, 4, 2);
+    let expected = "YES".to_string();
+    assert_eq!(real, expected);
+}
+
+#[test]
+fn test6_no() {
+    let real = kangaroo(0, 2, 5, 3);
+    let expected = "NO".to_string();
+    assert_eq!(real, expected);
+}
